56192138c9a1763229b1dcefb31e2851a1acf573
Nazar2903 <2222mnazar2222@gmail.com>
2026-05-20 14:01:15 +0300

    feat: add category_detail view to show recipes by category

--- recipe/views.py
+++ recipe/views.py
@@ -5,3 +5,7 @@ from .models import Recipe, Category
 def main(request):
     recipes = Recipe.objects.order_by('?')[:10]
     return render(request, 'main.html', {'recipes': recipes})
+
+def category_detail(request, id):
+    category = get_object_or_404(Category, id=id)
+    return render(request, 'category_detail.html', {'category': category})
