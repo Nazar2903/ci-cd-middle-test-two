850f4ebceddf69520340e64476481900d5527759
Nazar2903 <2222mnazar2222@gmail.com>
2026-05-20 14:05:12 +0300

    test: add test for main view functionality

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Category, Recipe
 
-# Create your tests here.
+
+class RecipeViewsTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Desserts")
+        for i in range(15):
+            Recipe.objects.create(
+                title=f"Recipe {i}",
+                description="Test Desc",
+                instructions="Test Inst",
+                ingredients="Test Ing",
+                category=self.category
+            )
+            
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
+        self.assertEqual(len(response.context['recipes']), 10)
