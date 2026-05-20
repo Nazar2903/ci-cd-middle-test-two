126c1681bd16c20c35fec0977a80f7e34e98d7bc
Nazar2903 <2222mnazar2222@gmail.com>
2026-05-20 14:00:33 +0300

    feat: add main view to return 10 random recipes

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,7 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe, Category
 
+
+def main(request):
+    recipes = Recipe.objects.order_by('?')[:10]
+    return render(request, 'main.html', {'recipes': recipes})
