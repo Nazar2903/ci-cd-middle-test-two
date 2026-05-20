5f63ee24f0b9b72103daa6784927fe4136c183a3
Nazar2903 <2222mnazar2222@gmail.com>
2026-05-20 14:03:19 +0300

    feat: setup url routing for recipe views

---
+++ recipe/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('category/<int:id>/', views.category_detail, name='category_detail'),
+]
